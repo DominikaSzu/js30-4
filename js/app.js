475235c53c72d191c79370a883e88cc5d2e288a2
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-16 20:33:24 +0200

    array.filter() task done

--- js/app.js
+++ js/app.js
@@ -17,6 +17,13 @@ document.addEventListener('DOMContentLoaded', function(e) {
     
     const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
     
+    // 1. Filter the list of inventors for those who were born in the 1500's
+    
+    const result1 = inventors.filter(inventor => (inventor.year > 1499 && inventor.year < 1600));
+    
+    console.log(result1);
+    
+    
     
     
 });
